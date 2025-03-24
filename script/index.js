1517d5ff5a1b6fe08d2f1a5d93c4005075b78e77
Rahmatullah <hrmilon1997@gmail.com>
2025-03-24 20:58:00 +0600

    added active class

--- script/index.js
+++ script/index.js
@@ -9,10 +9,21 @@ function loadCategory(){
     .then((data) => displayCategories(data.categories));
 }
 
+function removeActiveClass(){
+  const activeButtons = document.getElementsByClassName("active");
+  for(let btn of activeButtons){
+    btn.classList.remove("active");
+  }
+}
+
 function loadVideos(){
     fetch(" https://openapi.programming-hero.com/api/phero-tube/videos")
     .then((response) => response.json())
-    .then((data) => displayVideos(data.videos));
+    .then((data) => {
+      removeActiveClass();
+      document.getElementById("btn-all").classList.add("active");
+      displayVideos(data.videos)
+    });
 }
 
 function displayCategories(categories){
@@ -24,15 +35,42 @@ for(let cat of categories){
     // create Element 
     const categoryDiv = document.createElement("div");
     categoryDiv.innerHTML =`
-    <button class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`
+    <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>`
     // Append the Element
     categoryContainer.appendChild(categoryDiv);
 }
     
 }
 
+const loadCategoryVideos = (id) => {
+
+ const url = `https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
+ console.log(url);
+
+ fetch(url)
+ .then((res) => res.json())
+ .then((data) => {
+   removeActiveClass();
+   const clickedButton = document.getElementById(`btn-${id}`);
+   clickedButton.classList.add("active");
+   console.log(clickedButton);
+  displayVideos(data.category)
+  
+ });
+}
+
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
+ videoContainer.innerHTML = "";
+   if(videos.length == 0){
+    videoContainer.innerHTML = `
+    <div class=" py-20 col-span-full flex flex-col justify-center items-center text-center">
+            <img class="w-[120px]" src="assets/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+        </div>
+    `
+    return;
+   }
 
   videos.forEach((video) => {
    
@@ -56,7 +94,7 @@ const displayVideos = (videos) => {
                   </div>
               </div>
               <div class="intro">
-                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <h2 class="text-sm font-semibold">${video.title}</h2>
                 <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
                 <p class="text-sm text-gray-400">${video.others.views} views</p>
               </div>
@@ -69,4 +107,3 @@ const displayVideos = (videos) => {
 }
 
 loadCategory();
-loadVideos();
