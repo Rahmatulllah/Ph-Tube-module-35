f040f19401e5954ba6d31a6d060828156909a688
Rahmatullah <hrmilon1997@gmail.com>
2025-03-28 22:39:52 +0600

    added show details card

--- script/index.js
+++ script/index.js
@@ -58,6 +58,46 @@ const loadCategoryVideos = (id) => {
   
  });
 }
+const loadVideoDetails = (videoId) => {
+  console.log(videoId);
+  const url=`https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayVideoDetails(data.video));
+}
+
+const displayVideoDetails = (video) => {
+  console.log(video);
+  document.getElementById("video_details").showModal();
+  const detailsContainer = document.getElementById("details-container");
+  detailsContainer.innerHTML = `
+  <div class="card bg-base-100 image-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2>${video.title} </h2>
+    <div class="py-5 flex gap-3 px-0">
+              <div class="profile">
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-10 rounded-full ring ring-offset-2">
+                      <img src="${video.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+              </div>
+              <div class="intro">
+                <h2 class="text-sm font-semibold">${video.title}</h2>
+                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt=""></p>
+                <p class="text-sm text-gray-400">${video.others.views} views</p>
+              </div>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div>
+  `
+};
 
 const displayVideos = (videos) => {
   const videoContainer = document.getElementById("video-container");
@@ -99,6 +139,7 @@ const displayVideos = (videos) => {
                 <p class="text-sm text-gray-400">${video.others.views} views</p>
               </div>
             </div>
+            <button onclick=loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
           </div>
     `;
 
